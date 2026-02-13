2423a24daeb5cb129643958390cdf256e8f684d6
rafidz0012 <rafid892@gmail.com>
2026-02-13 18:31:49 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,23 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+Route::get('/test-notif', function () {
+
+    $token = "eX0sqJ5WSLCVQCsOn3IX7Y:APA91bEaNAXsUR6gOC9E42jD0CcZ7h-HjZU_eGTcS83YNgyhXQ57Zu3kxv_tpesHBsjLh9dNk1sZHt9tMRYiQ90ll6DR3pxrHM5ShJLWUQ3ZcjlQuDefob0";
+
+    $response = Http::withHeaders([
+        'Authorization' => 'key=SAAAAOj9fgDc:APA91bFT4cPM2QqWmYZEWcGK2hW9Yet-xAA4mmoPxn2Y9WMXNBM00FGZwPHnuuXKf6HKvIj1CaBHhajB6qvAL46Zi-ThV0JZZglbMdUhYkPUApJqXmivdIcXo8k50B2O4KLASruD5XFw',
+        'Content-Type' => 'application/json'
+    ])->post('https://fcm.googleapis.com/fcm/send', [
+        "to" => $token,
+        "notification" => [
+            "title" => "Test Laravel",
+            "body" => "Kalau bunyi berarti berhasil 🔔"
+        ]
+    ]);
+
+    return $response->body();
+});
 
 Route::post('/clip/callback', [ClipCallbackController::class, 'store']);
 
