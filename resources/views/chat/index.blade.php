d3145ce35b9b9e7ccfc13f9a6014fc00d7f9d3cd
rafidz0012 <rafid892@gmail.com>
2026-02-13 12:22:14 +0700

    f

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -125,7 +125,7 @@
                     <svg class="w-16 h-16 opacity-50" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                     </svg>
-                    <p>Select a user to start chatting</p>
+                    <p>pilih user unutk ngobrol</p>
                 </div>
             </template>
         </div>
